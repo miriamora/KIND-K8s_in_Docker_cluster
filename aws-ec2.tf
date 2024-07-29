cb58d0149fb92ddefea380e994252579af55266e
MiriamOra <miriamoranu@gmail.com>
2024-07-29 15:25:43 -0700

    update

---
+++ aws-ec2.tf
@@ -0,0 +1,98 @@
+# configured aws provider with proper credentials
+provider "aws" {
+  region    = var.aws_region
+  profile   = var.profile
+}
+
+# create default vpc if one does not exit
+resource "aws_default_vpc" "default_vpc" {
+}
+
+# use data source to get all avalablility zones in region
+data "aws_availability_zones" "available_zones" {}
+
+
+resource "aws_default_subnet" "default_az1" {
+  availability_zone = data.aws_availability_zones.available_zones.names[0]
+  tags   = {
+    Name = "utrains default subnet"
+  }
+}
+
+resource "aws_security_group" "kind_security_group" {
+  name        = "kind security group"
+  description = "allow access on ports 8080 and 22"
+  vpc_id      = aws_default_vpc.default_vpc.id
+
+  ingress {
+    description      = "http proxy access"
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  # allow access on port 22 ssh connection
+  ingress {
+    description      = "ssh access"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = -1
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags   = {
+    Name = "utrains jenkins server security group"
+  }
+}
+
+resource "aws_instance" "ec2_instance" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = var.aws_instance_type
+  subnet_id              = aws_default_subnet.default_az1.id
+  vpc_security_group_ids = [aws_security_group.kind_security_group.id]
+  key_name               = aws_key_pair.jenkins_key.key_name
+  user_data            = file("setup.sh")
+
+
+  tags = {
+    Name = "kind-k8s-server"
+  }
+}
+
+resource "null_resource" "example_provisioner" {
+  triggers = {
+    public_ip = aws_instance.ec2_instance.public_ip
+  }
+
+ connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = file(local_file.ssh_key.filename)
+    host        = aws_instance.ec2_instance.public_ip
+  }
+
+  // copy our example script to the server
+  provisioner "file" {
+    source      = "./installkind.sh"
+    destination = "/tmp/installkind.sh"
+  }
+
+  //Change permissions to executable and pipe its output into a new file
+  provisioner "remote-exec" {
+    inline = [
+      "sleep 30",
+      "dos2unix /tmp/installkind.sh",
+      "chmod +x /tmp/installkind.sh",
+      "bash /tmp/installkind.sh",
+    ]
+  }
+
+  depends_on = [aws_instance.ec2_instance]
+}
---
+++ installkind.sh
@@ -0,0 +1,54 @@
+#!/bin/bash -x
+
+## Install docker 
+
+sudo apt update
+sudo apt-get install docker.io -y
+sudo usermod -aG docker ubuntu
+sudo chmod 666 /var/run/docker.sock
+
+### Install de kind pour démarrer un cluster k8s dans des conteneurs docker
+
+
+curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.14.0/kind-linux-amd64
+chmod +x ./kind
+sudo mv ./kind /usr/bin/kind
+
+### Install kubectl => kubectl est un client en cli pour communiquer avec le cluster.
+
+
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/bin/kubectl
+
+### Generate config file
+
+cat <<EOF | tee kind.yml 
+kind: Cluster
+apiVersion: kind.x-k8s.io/v1alpha4
+nodes:
+  - role: control-plane
+    extraPortMappings:
+    - containerPort: 30001
+      hostPort: 30001
+  - role: worker
+  - role: worker
+EOF
+
+# Initialize cluster 
+
+# kind create cluster --name dev-cluster --config kind.yml
+kind create cluster --name dev-cluster --config kind.yml 2>&1 | tee output_logger.txt
+OUT_PUT_STRING=`cat output_logger.txt`
+
+BIGIN_STRING="cluster-info"
+END_STRING="Have"
+
+RESULT="${OUT_PUT_STRING#*${BIGIN_STRING}}"
+RESULT="${RESULT%${END_STRING}*}"
+
+echo $RESULT
+
+echo "--------> The config command is : kubectl cluster-info $RESULT"
+
+kubectl cluster-info$RESULT
